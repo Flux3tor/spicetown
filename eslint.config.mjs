d3f9cda3c2ef4e6b5aacd38579905eb521a58dec
Sabio <gamester.sad@gmail.com>
2025-12-24 08:55:41 +0000

    ok ok redo eslint.config.mjs because i imported shit from thin air

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+// why tf was i trying to import shit out of thin air
 
 export default [
   {
     languageOptions: {
+      sourceType: "module",
       globals: {
-        ...globals.browser,
-        ...globals.webextensions
+        window: "readonly",
+        document: "readonly",
+        chrome: "readonly",
+        browser: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        setInterval: "readonly"
       }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "error"
     }
-  },
-  pluginJs.configs.recommended,
+  }
 ];
